ea33691c36c15b3d6e5554f31d4af6bf71a79a73
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-03-03 16:52:10 +0100

    undepeloy

--- webconnector/src/main/java/i5/las2peer/connectors/webConnector/handler/ServicesHandler.java
+++ webconnector/src/main/java/i5/las2peer/connectors/webConnector/handler/ServicesHandler.java
@@ -235,9 +235,9 @@ public class ServicesHandler {
 	}
 
 	@POST
-	@Path("/undeployTest")
+	@Path("/stopDeployedServiceTEST")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response undeployTest(String body, @CookieParam(WebConnector.COOKIE_SESSIONID_KEY) String sessionId)
+	public Response stopDeployedServiceTEST(String body, @Context HttpHeaders httpHeaders)
 			throws Exception {
 		System.out.println(body);
 		JSONObject payload = parseJson(body);
@@ -246,21 +246,25 @@ public class ServicesHandler {
 		String link = payload.getAsString("link");
 		System.out.println(body);
 
-		AgentSession session = connector.getSessionById(sessionId);
-		if (session == null) {
-			throw new BadRequestException("You have to be logged in to upload");
-		} else if (pastryNode == null) {
+		// AgentSession session = connector.getSessionById(sessionId);
+		// if (session == null) {
+		// 	throw new BadRequestException("You have to be logged in to upload");
+		// } else 
+		if (pastryNode == null) {
 			throw new ServerErrorException(
 					"Service upload only available for " + PastryNodeImpl.class.getCanonicalName() + " Nodes",
 					Status.INTERNAL_SERVER_ERROR);
 		}
 
 		try {
-			System.out.println("UNDEPLOY NOOOOW");
-			System.out.println("UNDEPLOY NOOOOW");
-			System.out.println("UNDEPLOY NOOOOW");
-			System.out.println("UNDEPLOY NOOOOW");
-			PackageUploader.undeployTest(pastryNode, name, version, session.getAgent());
+			System.out.println("CCHHEECKK NOOOOW");
+			System.out.println("CCHHEECKK NOOOOW");
+			System.out.println("CCHHEECKK NOOOOW");
+			System.out.println("CCHHEECKK NOOOOW");
+			System.out.println("CCHHEECKK NOOOOW");
+			System.out.println("CCHHEECKK NOOOOW");
+			AgentImpl agent = authenticationManager.authenticateAgent(httpHeaders.getRequestHeaders(), "access-token");
+			PackageUploader.uploadServicePackageTest(pastryNode, name, version, agent, body);
 			JSONObject json = new JSONObject();
 			json.put("code", Status.OK.getStatusCode());
 			json.put("text", Status.OK.getStatusCode() + " - Service package upload successful");
@@ -273,6 +277,50 @@ public class ServicesHandler {
 			e.printStackTrace();
 			throw new BadRequestException("Service package upload failed", e);
 		}
+	}
+
+	@POST
+	@Path("/undeployTest")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response undeployTest(String body)
+			throws Exception {
+		System.out.println(body);
+		JSONObject payload = parseJson(body);
+		String name = payload.getAsString("name");
+		String version = payload.getAsString("version");
+		// String link = payload.getAsString("link");
+		// System.out.println(body);
+
+		// AgentSession session = connector.getSessionById(sessionId);
+		// if (session == null) {
+		// 	throw new BadRequestException("You have to be logged in to upload");
+		// } else if (pastryNode == null) {
+		// 	throw new ServerErrorException(
+		// 			"Service upload only available for " + PastryNodeImpl.class.getCanonicalName() + " Nodes",
+		// 			Status.INTERNAL_SERVER_ERROR);
+		// }
+
+		// try {
+			ethereumNode.stopService(ServiceNameVersion.fromString(name + "@" + version));
+			return Response.ok().build();
+
+			// System.out.println("UNDEPLOY NOOOOW");
+			// System.out.println("UNDEPLOY NOOOOW");
+			// System.out.println("UNDEPLOY NOOOOW");
+			// System.out.println("UNDEPLOY NOOOOW");
+			// PackageUploader.undeployTest(pastryNode, name, version, session.getAgent());
+			// JSONObject json = new JSONObject();
+			// json.put("code", Status.OK.getStatusCode());
+			// json.put("text", Status.OK.getStatusCode() + " - Service package upload successful");
+			// json.put("msg", "Service package upload successful");
+			// return Response.ok(json.toJSONString(), MediaType.APPLICATION_JSON).build();
+		// } catch (EnvelopeAlreadyExistsException e) {
+		// 	throw new BadRequestException("Version is already known in the network. To update increase version number",
+		// 			e);
+		// } catch (ServicePackageException e) {
+		// 	e.printStackTrace();
+		// 	throw new BadRequestException("Service package upload failed", e);
+		// }
 	}
 	
 	@POST
